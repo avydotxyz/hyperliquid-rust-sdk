bf56848461f31463f9762b04a775c8336d08fbb8
marc <me@falumpaset.com>
2025-08-07 15:23:01 +0000

    remove warnings

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,6 @@ pub mod ws;
 pub use consts::{EPSILON, LOCAL_API_URL, MAINNET_API_URL, TESTNET_API_URL};
 pub use errors::Error;
 pub use exchange::*;
-pub use helpers::{bps_diff, truncate_float, BaseUrl};
+pub use helpers::{bps_diff, truncate_float};
 pub use info::*;
 pub use ws::*;
